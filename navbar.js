cb71ca00034091ec5781e06034e395202c0c15a0
nubtk_portal <111191462+passorbit@users.noreply.github.com>
2026-06-13 00:48:47 +0600

    Update navbar.js
    Add games on navbar

--- navbar.js
+++ navbar.js
@@ -19,6 +19,7 @@ const navbarHTML = `
       <a href="${pagesPrefix}routine.html" class="nav-link">Routine</a>
       <a href="${pagesPrefix}teachers.html" class="nav-link">Teacher's Directory</a>
       <a href="${pagesPrefix}index-maker.html" class="nav-link">Index Maker</a>
+      <a href="${pagesPrefix}games.html" class="nav-link">Games</a>
       <a href="${pagesPrefix}admin.html" class="nav-link nav-admin">Admin</a>
     </div>
     
